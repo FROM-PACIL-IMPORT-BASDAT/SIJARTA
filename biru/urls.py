4453b11c45c0504c3f8cdd976463f9809e6b1f3a
kaindraa <143985776+kaindraa@users.noreply.github.com>
2024-11-12 10:37:10 +0700

    templates diskon

--- biru/urls.py
+++ biru/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import testimoni
+from .views import testimoni, diskon
 
 app_name = 'biru'
 
 urlpatterns = [
     path('testimoni/', testimoni, name='testimoni'),
+    path('diskon/', diskon, name='diskon'),
+
 ]
--- biru/views.py
+++ biru/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def testimoni(request):
     return render(request, 'testimoni.html')
+
+def diskon(request):
+    return render(request, 'diskon.html')
